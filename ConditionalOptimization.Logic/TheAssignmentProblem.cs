947938492cf6e9a1faf9658f1b2c1c4981f3d1d8
Алексей <97665299+0ne290@users.noreply.github.com>
2023-11-10 15:49:02 +0800

    Add TheAssignmentProblema

---
+++ ConditionalOptimization.Logic/TheAssignmentProblem.cs
@@ -0,0 +1,14 @@
+public class TheAssignmentProblema
+{
+  public TheAssignmentProblem(double[] assignmentTable)
+  {
+    AssignmentTable = assignmentTable;
+  }
+  
+  public void HungarianAlgorithm()
+  {
+    
+  }
+
+  public double[] AssignmentTable { get; set; }
+}
