3ae008603b28086af1cc08fffb124f33dd62b45f
Алексей <97665299+0ne290@users.noreply.github.com>
2023-11-15 22:15:58 +0800

    Update Vertex.cs

--- ConditionalOptimization.Logic/Vertex.cs
+++ ConditionalOptimization.Logic/Vertex.cs
@@ -6,41 +6,27 @@ public class Vertex
 	{
 		Id = _counter;
 		Visited = false;
-		_outgoingEdges = new List<Edge>(10);
-		_incomingEdges = new List<Edge>(10);
+		_edges = new List<Edge>(10);
 		_counter++;
 	}
 
-	public void AddOutgoingEdge(Edge edge)
+	public void AddEdge(Edge edge)
 	{
-		if (_outgoingEdges.Contains(edge))
+		if (_edges.Contains(edge))
 			throw new Exception("The specified edge already originates from the vertex");
 		_outgoingEdges.Add(edge);
-	}
-	public void AddIncomingEdge(Edge edge)
-	{
-		if (_incomingEdges.Contains(edge))
-			throw new Exception("The specified edge is already included in the vertex");
-		_incomingEdges.Add(edge);
 	}
 	
 	public void DeleteOutgoingEdge(Edge edge)
 	{
-		if (_outgoingEdges.Remove(edge))
+		if (!_edges.Remove(edge))
 			throw new Exception("The specified edge does not leave a vertex");
 	}
-	public void DeleteIncomingEdge(Edge edge)
-	{
-		if (_incomingEdges.Remove(edge))
-			throw new Exception("The specified edge of the graph is included in the vertex");
-	}
 
-	public List<Edge> OutgoingEdges => new(_outgoingEdges);
-	public List<Edge> IncomingEdges => new(_incomingEdges);
+	public List<Edge> Edges => new(_edges);
 	public int Id { get; }
 	public bool Visited { get; set; }
 	
-	private List<Edge> _outgoingEdges;
-	private List<Edge> _incomingEdges;
+	private List<Edge> edges;
 	private static int _counter = 1;
 }
