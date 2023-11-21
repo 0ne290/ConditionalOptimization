9565c5720e63465f9a27b802771ad260ce9f2074
Алексей <97665299+0ne290@users.noreply.github.com>
2023-11-21 21:02:26 +0800

    Work

--- ConditionalOptimization.Logic/Edge.cs
+++ ConditionalOptimization.Logic/Edge.cs
@@ -1,13 +1,25 @@
 namespace ConditionalOptimization.Logic;
 
-public class Edge
-{
-    public Edge(int capacity = 1, int flow = 0)
-    {
-        Capacity = capacity;
-        Flow = flow;
-    }
-
-    public int Capacity { get; }
-    public int Flow { get; set; }
+public class Edge 
+{ 
+ public Edge(int capacity) => Capacity = capacity; 
+ 
+ public void SendFlow(int flow) => Capacity -= flow; 
+ public void ReceiveFlow(int flow) => Capacity += flow; 
+ 
+ public bool IsBusy() => _capacity < 1; 
+ public bool IsFree() => _capacity > 0; 
+ 
+ public int Capacity 
+ { 
+ get => _capacity; 
+ private set 
+ { 
+ if (value < 0) 
+ throw new Exception("Capacity cannot be less than zero"); 
+ _capacity = value; 
+ } 
+ } 
+ 
+ private int _capacity; 
 }
