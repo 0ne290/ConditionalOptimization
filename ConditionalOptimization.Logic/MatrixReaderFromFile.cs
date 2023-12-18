d5741e99f1338d74f3402ce7087d90b5a24a1eb6
Алексей <97665299+0ne290@users.noreply.github.com>
2023-12-18 23:47:41 +0800

    Work

--- ConditionalOptimization.Logic/MatrixReaderFromFile.cs
+++ ConditionalOptimization.Logic/MatrixReaderFromFile.cs
@@ -2,38 +2,30 @@ namespace ConditionalOptimization.Logic;
 
 public static class MatrixReaderFromFile
 {
-    public static async Task<double[,]> LoadMatrix(string pathToFileMatrix)
+    public static double[,] LoadMatrix(string pathToFileMatrix)
     {
         var rowIndex = 0;
-        using var streamReader = File.OpenText(pathToFileMatrix);
+        var rows = File.ReadAllLines(pathToFileMatrix);
 
-        var rows = await LoadRow(streamReader);
         var dimension = rows.Count;
         var matrix = new double[dimension, dimension];
         
         foreach (var row in rows)
         {
-            if (row.Count != dimension)
+            var numbers = row.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                    .Select(double.Parse);
+            
+            if (numbers.Count != dimension)
                 throw new Exception("The cost table must be square.");
             
             var columnIndex = 0;
-            foreach (var element in row)
-                matrix[rowIndex, columnIndex++] = element;
+            
+            foreach (var number in numbers)
+                matrix[rowIndex, columnIndex++] = number;
+            
             rowIndex++;
         }
  	
         return matrix;
-    }
-    
-    private static async IAsyncEnumerable<IEnumerable<double>> LoadRow(StreamReader reader)
-    {
-        while (!reader.EndOfStream)
-        {
-            var line = await reader.ReadLineAsync();
-            if (!string.IsNullOrEmpty(line))
-                yield return line
-                    .Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
-                    .Select(double.Parse);
-        }
     }
 }
